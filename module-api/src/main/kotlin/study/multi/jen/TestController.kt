59a8c6bede2a602841b412742799716f11ff49ff
its-sky <smc9919@naver.com>
2024-04-04 00:56:26 +0900

    test: Jenkins push webhook 테스트

---
+++ module-api/src/main/kotlin/study/multi/jen/TestController.kt
@@ -0,0 +1,16 @@
+package study.multi.jen
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class TestController {
+
+    @GetMapping
+    fun checkHealth(): String {
+        return "OK"
+    }
+
+}
