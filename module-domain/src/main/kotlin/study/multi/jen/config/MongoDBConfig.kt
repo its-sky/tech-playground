f4ffdf1f9c660b9fcb671f817a8224e152d5e2ea
its-sky <smc9919@naver.com>
2024-04-17 18:08:40 +0900

    fix: MongoDBConfig kotlin class로 변경

--- module-domain/src/main/kotlin/study/multi/jen/config/MongoDBConfig.kt
+++ module-domain/src/main/kotlin/study/multi/jen/config/MongoDBConfig.kt
@@ -1,12 +1,11 @@
-package study.multi.jen.config;
+package study.multi.jen.config
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @Configuration
-@EnableAutoConfiguration(exclude = MongoAutoConfiguration.class)
-@EnableMongoRepositories(basePackages = "study.multi.test")
-public class MongoDBConfig {
-}
+@EnableAutoConfiguration(exclude = [MongoAutoConfiguration::class])
+@EnableMongoRepositories(basePackages = ["study.multi.jen.mongo"])
+class MongoDBConfig
